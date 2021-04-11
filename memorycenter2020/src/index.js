9c8341a19d8684be31b26b5ead67c5e36cee120b
Fariax-code <est.jhoseph.farias@unimilitar.edu.co>
2021-04-11 14:11:30 -0500

    Fullpage

--- memorycenter2020/src/index.js
+++ memorycenter2020/src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+/* eslint-disable import/no-extraneous-dependencies */
+import React from "react";
+import ReactDOM from "react-dom";
+import "fullpage.js/vendors/scrolloverflow"; // Optional. When using scrollOverflow:true
+import ReactFullpage from "@fullpage/react-fullpage";
 
+// import "./styles.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+class MySection extends React.Component {
+  render() {
+    return (
+      <div className="section">
+        <h3>{this.props.content}</h3>
+      </div>
+    );
+  }
+}
+
+const anchors = ["firstPage", "secondPage", "thirdPage"];
+
+const FullpageWrapper = () => (
+  <ReactFullpage
+    anchors={anchors}
+    navigation
+    navigationTooltips={anchors}
+    sectionsColor={["#282c34", "#ff5f45", "#0798ec"]}
+    onLeave={(origin, destination, direction) => {
+      console.log("onLeave event", { origin, destination, direction });
+    }}
+    render={({ state, fullpageApi }) => {
+      console.log("render prop change", state, fullpageApi); // eslint-disable-line no-console
+
+      return (
+        <div>
+          <MySection content={"Slide down!"} />
+          <MySection content={"Keep going!"} />
+          <MySection content={"Slide up!"} />
+        </div>
+      );
+    }}
+  />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(<FullpageWrapper />, document.getElementById("root"));
 
